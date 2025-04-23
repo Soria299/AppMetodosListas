4055b36e4b162e6d1222f9e4c212d46a831abb96
HP <HP@JESICA-SORIA>
2025-04-23 02:51:10 -0500

    Agregar archivos de proyecto.

---
+++ AppMetodosListas/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace AppMetodosListas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtElemento = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.lstElementos = new System.Windows.Forms.ListBox();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtElemento
+            // 
+            this.txtElemento.Location = new System.Drawing.Point(10, 57);
+            this.txtElemento.Name = "txtElemento";
+            this.txtElemento.Size = new System.Drawing.Size(100, 20);
+            this.txtElemento.TabIndex = 0;
+            this.txtElemento.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtElemento_KeyPress);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(159, 39);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 1;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // lstElementos
+            // 
+            this.lstElementos.FormattingEnabled = true;
+            this.lstElementos.Location = new System.Drawing.Point(269, 28);
+            this.lstElementos.Name = "lstElementos";
+            this.lstElementos.Size = new System.Drawing.Size(120, 95);
+            this.lstElementos.TabIndex = 2;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(159, 81);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 3;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(409, 201);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.lstElementos);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtElemento);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtElemento;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.ListBox lstElementos;
+        private System.Windows.Forms.Button btnEliminar;
+    }
+}
+
---
+++ AppMetodosListas/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppMetodosListas
+{
+    public partial class Form1 : Form
+    {
+        // Lista para almacenar elementos 
+
+        private List<string> elementos = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+            txtElemento.KeyPress += new KeyPressEventHandler(txtElemento_KeyPress);
+        }
+
+        // Método para agregar elementos 
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            string nuevoElemento = txtElemento.Text;
+            if (!string.IsNullOrWhiteSpace(nuevoElemento) && SoloContienLetras(nuevoElemento))
+            {
+                elementos.Add(nuevoElemento);
+                ActualizarLista();
+                txtElemento.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Por favor ingresa solo letras.");
+            }
+        }
+        private bool SoloContienLetras(string texto)
+        {
+            // Verifica que cada carácter sea una letra o un espacio
+            foreach (char c in texto)
+            {
+                if (!char.IsLetter(c) && c != ' ')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        // Método para eliminar elementos seleccionados 
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (lstElementos.SelectedItem != null)
+            {
+                elementos.Remove(lstElementos.SelectedItem.ToString());
+                ActualizarLista();
+            }
+            else
+            {
+                MessageBox.Show("Selecciona un elemento para eliminar.");
+            }
+        }
+
+        // Método para actualizar ListBox 
+        private void ActualizarLista()
+        {
+            lstElementos.DataSource = null;
+            lstElementos.DataSource = elementos;
+        }
+        private void txtElemento_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Permitir solo letras (mayúsculas y minúsculas), la tecla de retroceso y espacios
+            if (!char.IsLetter(e.KeyChar) && e.KeyChar != (char)Keys.Back && e.KeyChar != ' ')
+            {
+                e.Handled = true; // Cancela la entrada del carácter
+            }
+        }
+    }
+}
